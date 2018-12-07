ce6650f3b778fbb24e9cd54eeac254c9dd046e6b
Domingo Brandt <brandtyet02@gmail.com>
2018-12-07 13:20:26 -0400

    blade extend yield

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -1,9 +1,57 @@
+@extends('layouts.app')
+@section('title','Employees Create')
+
+@section('content')
+<div class="from-group">
+    <label for="">Nombre</label>
+    <input type="text" class="from-control">
+</div>
+
+<div class="from-group">
+    <label for="">foto</label>
+    <input type="text" class="from-control">
+</div>
+
+<div class="from-group">
+    <label for="">Descripcion</label>
+    <input type="text" class="from-control">
+</div>
+
+<div class="from-group">
+    <label for="">user id</label>
+    <select class="from-control"></select>
+</div>
+<button type="submit" class="btn btn-primary">Guardar</button>
+</div>
+@endsection
+{{--
 <html>
     <head>
         <title></title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     </head>
     <body>
+        <div class="container">
+            <div class="from-group">
+                <label for="">Nombre</label>
+                <input type="text" class="from-control">
+            </div>
 
+            <div class="from-group">
+                <label for="">foto</label>
+                <input type="text" class="from-control">
+            </div>
+
+            <div class="from-group">
+                <label for="">Descripcion</label>
+                <input type="text" class="from-control">
+            </div>
+
+            <div class="from-group">
+                <label for="">user id</label>
+                <select class="from-control"></select>
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </div>
     </body>
-</html>
+</html>--}}
